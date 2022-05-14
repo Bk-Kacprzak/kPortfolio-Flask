38db24d961b1855fcdc9498756aeaf0a10e5d74c
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-05-14 12:27:22 +0200

    add CSRF protection

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from app.__init__ import create_app
+from flask_wtf.csrf import CSRFProtect
 
 if __name__ == "__main__":
     app = create_app()
+    csrf = CSRFProtect()
+    csrf.init_app(app)
     app.run(port = 4000)
