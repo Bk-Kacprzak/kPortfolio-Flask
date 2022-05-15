214290fbc920eb68927dfd0aa0baf0c97363950a
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-05-15 16:17:51 +0200

    Add AssetForm

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,15 +1,16 @@
 from flask_wtf import FlaskForm
-from flask_wtf import FlaskForm
-from wtforms import StringField, EmailField, SubmitField, TextAreaField, PasswordField, FormField, IntegerField
-from wtforms.validators import DataRequired, Email, EqualTo, InputRequired, Length, NumberRange
-
+from wtforms import StringField, SubmitField, DateTimeField, FloatField, SelectField
+from wtforms.validators import DataRequired, NumberRange
 
 class PortfolioForm(FlaskForm) : 
     portfolio_name = StringField("PORTFOLIO NAME", validators=[DataRequired()])
-    total_cost = IntegerField("TOTAL COST", validators = [NumberRange(1, 1000000)])
+    total_cost = FloatField("TOTAL COST", validators = [NumberRange(1, 1000000)])
     submit = SubmitField("SAVE")
 
-
-
 class AssetForm(FlaskForm) : 
-    pass 
+    portfolio = SelectField("PORTFOLIO NAME")
+    asset_name = StringField("Asset Name", validators=[DataRequired()])
+    amount = FloatField("TOTAL COST")
+    date = DateTimeField("Date",format='%Y-%m-%d %H:%M:%S')
+    price = FloatField("PRICE", validators=[DataRequired()])
+    submit = SubmitField("ADD")
