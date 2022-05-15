52568233f2b06a0b2e73e9596e9990f81ea3243d
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-05-15 16:33:41 +0200

    Refactor button click listener functions

--- app/static/events.js
+++ app/static/events.js
@@ -1,17 +1,25 @@
-const button = document.querySelector("button.add-button");
-const newPortfolioForm = document.querySelector(".form-container");
+// const button = document.querySelector("button.add-button");
+// const newPortfolioForm = document.querySelector(".form-container");
 const mainContainer = document.querySelector(".content-container");
 const cancelButton = document.querySelector(".cancel-button");
 
-
-button.addEventListener('click', () => {
-    console.log(newPortfolioForm);
+const showForm = (formToOpenClass, anotherFormClass = null) => { 
+    if (anotherFormClass != null) { 
+        anotherForm = document.querySelector(anotherFormClass);
+        if (anotherForm.style.display == "block") 
+            return;
+    }
+  
+    form = document.querySelector(formToOpenClass) 
     mainContainer.setAttribute("style", "opacity: 0.2");
-    newPortfolioForm.setAttribute("style", "display: block");
-});
+    form.setAttribute("style", "display: block");
+}
 
-
-cancelButton.addEventListener('click', () => { 
+const hideForm = (formClasses) => { 
+    formClasses.forEach((value)=> { 
+        form = document.querySelector(value) 
+        form.setAttribute("style", "display: none");
+    })
+    // form = document.querySelector(formClass) 
     mainContainer.setAttribute("style", "opacity: 1");
-    newPortfolioForm.setAttribute("style", "display: none");
-});
+}
