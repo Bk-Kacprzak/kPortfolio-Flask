a4986bac1b584027d6860e0a1dd113802c3ada08
Bartlomiej Kacprzak <bk.kacprzak@gmail.com>
2022-05-17 13:48:35 +0200

    Refactor columns of Asset model

--- app/models/asset.py
+++ app/models/asset.py
@@ -1,28 +1,39 @@
 from app.sqla import sqla 
 from app.api.api_coingecko import cg
+# import numpy as np 
 class Asset(sqla.Model) : 
     id = sqla.Column(sqla.Integer, primary_key = True) 
+    portfolio_id = sqla.Column(sqla.Integer, sqla.ForeignKey("portfolio.id"), nullable = False) 
+
     name = sqla.Column(sqla.Text, nullable = False)
-    transaction_date = sqla.Column(sqla.DateTime, nullable = False)
-    entry_price = sqla.Column(sqla.Integer, nullable = False)
-
-
-    portfolio_id = sqla.Column(sqla.Integer, sqla.ForeignKey("Portfolio.id"), nullable = False) 
+    cost = sqla.Column(sqla.Float, nullable = False)
+    average_buy_price = sqla.Column(sqla.Float, nullable = False)
+    total_amount = sqla.Column(sqla.Float)
+    average_sell_price = sqla.Column(sqla.Float) 
 
     portfolio = sqla.relationship('Portfolio', backref = sqla.backref('assets'))
 
-    
-    # portfolio_id = sqla.Column(sqla.Integer, sqla.ForeignKey("Portfolio.id"), nullable = False)
-    # portoflio = sqla.relationship("Portfolio", backref= sqla.backref('portfolios', lazy = True))
-
-    # user_id = sqla.Column(sqla.Integer, sqla.ForeignKey('user.id'), nullable = False) 
-
     def count_profit(self) : 
         return self.entry_price - cg.get_current_price(self.name)
-    
-    # TODO :
-    # 
+
 
     def count_historical_profit(self, datetime) :
         return self.entry_price - cg.get_historical_price(datetime)
     
+    # def count_average_buy_price(self, current_price) :
+        
+    def update_holdings(self, amount, transaction_type, current_price) : 
+        if transaction_type == "SELL":
+            amount = (-1)*amount
+            # self.average_sell_price = np.average([self.average_sell_price, current_price], weights = [total_amount, amount])
+        
+        if transaction_type == "BUY": 
+            pass
+            # self.average_buy_price = np.average([self.average_buy_price, current_price], weights = [total_amount, amount])
+
+            
+        self.total_amount = self.total_amount + amount
+
+
+
+            
